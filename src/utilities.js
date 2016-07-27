844bb8afec69039bb664d3383c674a5ae3395bc6
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-07-27 13:43:33 -0600

    Utilities file for functions like adding/removing classes, etc.

---
+++ src/utilities.js
@@ -0,0 +1,26 @@
+import 'core-js/fn/array/includes';
+
+export function addEvent(element, eventName, callback) {
+	element.addEventListener(eventName, callback);
+}
+
+export function removeEvent(element, eventName, callback) {
+	element.removeEventListener(eventName, callback);
+}
+
+export function addClass(element, ...classes) {
+	var currentClasses = element.className.trim().split(/\s+/);
+	element.className = currentClasses
+		.concat(
+			classes.filter(cls => !currentClasses.includes(cls.trim()))
+		)
+		.join(' ')
+		.trim();
+}
+
+export function removeClass(element, ...classes) {
+	element.className = element.className.trim().split(/\s+/)
+		.filter(cls => !classes.includes(cls.trim()))
+		.join(' ')
+		.trim();
+}
