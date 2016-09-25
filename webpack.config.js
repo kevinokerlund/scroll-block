f814bee4b2222ad11961e32e02a37b0f0b01b3da
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-09-25 15:33:12 -0600

    Simplify build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,49 @@
 var webpack = require('webpack');
-var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-var path = require('path');
-var env = require('yargs').argv.mode;
-var libraryName = 'ScrollBlock';
-
-var plugins = [], outputFile;
-
-if (env === 'build') {
-	plugins.push(new UglifyJsPlugin({minimize: true}));
-	outputFile = 'scroll-block.min.js';
-} else {
-	outputFile = 'scroll-block.js';
-}
-
 
 module.exports = {
 	entry: __dirname + "/src/scroll-block.js",
 	devtool: 'source-map',
 	output: {
-		path: __dirname + '/lib',
-		filename: outputFile,
-		library: libraryName,
+		path: './lib',
+		filename: 'scroll-block.js',
+		library: 'ScrollBlock',
 		libraryTarget: 'umd',
 		umdNamedDefine: true
 	},
 	module: {
 		loaders: [
 			{
+				loader: "babel",
 				test: /\.js$/,
 				exclude: /node_modules/,
-				loader: "babel-loader"
+				query: {
+					presets: ['es2015'],
+					plugins: [
+						'babel-plugin-add-module-exports',
+						'transform-object-rest-spread'
+					]
+				}
 			},
 			{
+				loader: "html-loader",
 				test: /\.html$/,
-				exclude: /node_modules/,
-				loader: "html-loader"
+				exclude: /node_modules/
 			},
 			{
+				loader: "style-loader!css-loader",
 				test: /\.css$/,
-				exclude: /node_modules/,
-				loader: "style-loader!css-loader"
+				exclude: /node_modules/
 			}
 		]
 	},
-	resolve: {
-		root: path.resolve('./src'),
-		extensions: ['', '.js']
-	},
-	plugins: plugins
+	plugins: [
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false,
+			},
+			output: {
+				comments: false,
+			},
+		}),
+	]
 };
